31626db31c360734d834bc0e63a514298e4c8daa
HouseWife <chandu.thrilok@gmail.com>
2025-12-01 16:16:56 +0530

    Update nse_options_analyzer.py

--- nse_options_analyzer.py
+++ nse_options_analyzer.py
@@ -55,8 +55,8 @@ if 'overall_option_data' not in st.session_state:
     st.session_state['overall_option_data'] = {}
 
 # === Telegram Config ===
-TELEGRAM_BOT_TOKEN = "8133685842:AAGdHCpi9QRIsS-fWW5Y1AJvS95QL9xU"
-TELEGRAM_CHAT_ID = "57096584"
+TELEGRAM_BOT_TOKEN = "8133685842:AAGdHCpi9QRIsS-fWW5Y1ArgKJvS95QL9xU"
+TELEGRAM_CHAT_ID = "5704496584"
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
